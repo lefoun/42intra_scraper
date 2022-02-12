9a60f3b5dadfb9af4a68eddf1c95623dc83ecc2f
Noufel <noufel.ammari@hotmail.com>
2022-02-12 01:59:10 +0100

    added get pass to hide user information

--- intra_scraper.py
+++ intra_scraper.py
@@ -4,6 +4,7 @@ from os import getcwd, listdir
 from sys import argv
 import html5lib
 from utils import create_dir, display_message, COLORS
+from getpass import getpass
 
 
 def get_mp4_link(video_tag: BeautifulSoup) -> str:
@@ -94,7 +95,7 @@ if __name__ == "__main__":
     INTRA_PREFIX_LINK = "https://elearning.intra.42.fr"
     LOGIN_LINK = "https://signin.intra.42.fr/users/sign_in"
     USER_NAME = input("Please enter your 42 user_name: ")
-    USER_PASSWORD = input("\nPlease enter your 42 password: ")
+    USER_PASSWORD = getpass("\nPlease enter your 42 password: ")
     USER_LINK = input(
         "\nPlease enter the link to the page you want to "
         f"scrap. Otherwise click 'enter' to defautl to:\n{E_LEARNING_LINK} ")
